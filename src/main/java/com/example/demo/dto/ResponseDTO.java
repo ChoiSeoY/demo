0abc5d9b60c48bd86847acb720d9219e1b48dccb
Choi SeoYoung <chltjdud30@naver.com>
2023-06-17 19:59:31 +0900

    백엔드 개발 13. @ResponseBody 사용하기

---
+++ src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class ResponseDTO<T>{
+	private String error;
+	private List<T> data;
+	
+}
+
